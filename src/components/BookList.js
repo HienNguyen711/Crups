0b0506de55734bc55635256ab59305647a8dbbb1
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 13:07:52 +0200

    create BookList component

---
+++ src/components/BookList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import BookPreview from './BookPreview';
+
+const BookList = ({ books, onBookClick }) => (
+  <div className="BookList">
+    {Object.keys(books).map(bookId =>
+      <BookPreview
+        key={bookId}
+        onClick={onBookClick}
+        {...books[bookId]} />
+    )}
+  </div>
+);
+
+BookList.propTypes = {
+  books: React.PropTypes.object,
+  onBookClick: React.PropTypes.func.isRequired,
+};
+
+export default BookList;
