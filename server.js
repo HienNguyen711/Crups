1608b5cd448c09612a3dfa087b56c35fe299c730
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 11:06:39 +0200

    updating sever.js

---
+++ server.js
@@ -0,0 +1,13 @@
+import config from './config';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const server = express();
+server.use(bodyParser.json());
+
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
