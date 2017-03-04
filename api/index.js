447afe3034630c374d19f5e69db7e511d54ea6e6
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 15:08:05 +0200

    update api endpoints for routes :octocat:

--- api/index.js
+++ api/index.js
@@ -1,24 +1,87 @@
 import express from 'express';
-import data from '../src/data';
+import { MongoClient, ObjectID } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
+
+
+let mdb;
+MongoClient.connect(config.mongodbUri, (err, db) => {
+  assert.equal(null, err);
+
+  mdb = db;
+});
 
 const router = express.Router();
-const books = data.books.reduce((obj, book) => {
-  obj[book.id] = book;
-  return obj;
-}, {});
+
 
 router.get('/books', (req, res) => {
-  res.send({
-    books: books//books
+   let books = {};
+  mdb.collection('books').find({})
+     .project({
+       categoryName: 1,
+       bookName: 1
+     })
+     .each((err, book) => {
+       assert.equal(null, err);
+
+       if (!book) { // no more books
+         res.send({ books });
+         return;
+       }
+
+       books[book._id] = book;
+     });
+});
+router.get('/readers/:readerIds', (req, res) => {
+  const readerIds = req.params.readerIds.split(',').map(ObjectID);
+  let readers = {};
+  mdb.collection('readers').find({ _id: { $in: readerIds }})
+     .each((err, reader) => {
+       assert.equal(null, err);
+
+       if (!reader) { // no more readers
+         res.send({ readers });
+         return;
+       }
+
+       readers[reader._id] = reader;
+     });
+});
+
+
+router.get('/books/:bookId', (req, res) => {
+  mdb.collection('books')
+     .findOne({ _id: ObjectID(req.params.bookId) })
+     .then(book => res.send(book))
+     .catch(error => {
+       console.error(error);
+       res.status(404).send('Bad Request');
+     });
+});
+
+
+router.post('/readers', (req, res) => {
+  const bookId = ObjectID(req.body.bookId);
+  const reader = req.body.newReader;
+  // validation ...
+  mdb.collection('readers').insertOne({ reader }).then(result =>
+    mdb.collection('books').findAndModify(
+      { _id: bookId },
+      [],
+      { $push: { readerIds: result.insertedId } },
+      { new: true }
+    ).then(doc =>
+      res.send({
+        updatedBook: doc.value,
+        newReader: { _id: result.insertedId, reader }
+      })
+    )
+  )
+  .catch(error => {
+    console.error(error);
+    res.status(404).send('Bad Request');
   });
 });
 
-router.get('/books/:bookId', (req, res) => {
-  let book = books[req.params.bookId];
-  book.description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-
-
-  res.send(book);
-});
-
 export default router;
+
