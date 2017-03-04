ff6018040f81a6fe3566069b19c5155cc12c5d0d
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 11:24:46 +0200

    create App componennt

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+class App extends Reaac.Component {
+   render() {
+    return (
+      <div className="App">
+        <Header />
+
+      </div>
+    );
+  }
+}
+
+export default App;
