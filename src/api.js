8ad657973dad8b98b35d464225afb0e69922a548
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-04 15:11:42 +0200

    updating api.js for names

--- src/api.js
+++ src/api.js
@@ -9,3 +9,13 @@ export const fetchBookList = () => {
   return axios.get('/api/books')
               .then(resp => resp.data.books);
 };
+
+export const fetchReaders = readerIds => {
+  return axios.get(`/api/readers/${readerIds.join(',')}`)
+              .then(resp => resp.data.readers);
+};
+
+export const addReader = (newReader, bookId) => {
+  return axios.post('/api/readers', { newReader, bookId })
+              .then(resp => resp.data);
+};
